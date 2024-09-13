cae9601a8eb9817afd87f8e68bef5abf7ebe9b91
Overmind <overmind@MacBook-Pro-Overmind.local>
2024-09-13 13:25:46 +0300

    add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react';
+import { socket } from 'socket/socket';
+
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  const sendMessage = () => {
+    if (title && text) {
+      socket.emit('sendMessage', { title, text });
+      setTitle('');
+      setText('');
+    }
+  };
+
+  const hanleSubmit = e => {
+    e.preventDefault();
+    sendMessage();
+  };
+
   return (
     <div className="App">
-      <div className="form-wrapper">
-        
-      </div>
+      <h1>Send message</h1>
+      <form className="form" onSubmit={hanleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={e => {
+            setTitle(e.target.value);
+          }}
+        />
+        <textarea
+          name=""
+          id=""
+          placeholder="Message"
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+        ></textarea>
+      </form>
     </div>
   );
 }
